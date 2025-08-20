3571600eb02decc105b31a54baa6a93db72e70e6
nattapong <azzzx30089@gmail.com>
2025-08-20 23:43:21 +0700

    added home

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import { FaBook } from "react-icons/fa";
 
-const Home = () => {
+export default function Page() {
+
+  
+
   return (
     <>
-    {/* <h1>ยินดีต้อนรับ</h1> */}
+      <div className="bg-primary p-5 rounded-2xl">
+        <div className="flex justify-between items-center">
+          <div>
+            <h1 className="text-white text-2xl">สวัสดีคุณ, Nattapong Nakaom</h1>
+            <p className="text-gray-300 text-xl mt-1">20000-9999 การเขียนโปรแกรมไพธอนเบื้องต้น</p>
+            <p className="text-gray-300 text-xl mt-1">ปวช.1 เทคโนโลยีสารสนเทศ</p>
+          </div>
+          <div>
+            <button className="bg-white text-primary rounded-xl  px-5 py-1.5">ออกจากระบบ</button>
+          </div>
+        </div>
+      </div>
+      <div className="mt-5 ms-2 mb-3">
+        <h2 className="text-2xl font-bold ">งานที่ได้รับมอบหมาย</h2>
+      </div>
+      <div className="bg-white p-5 rounded-xl border border-gray-300 cursor-pointer hover:shadow-md duration-300 transition-all">
+        <div className="flex items-center gap-5">
+          <div className="p-4 bg-gray-200 rounded-full">
+            <FaBook className="text-2xl" />
+          </div>
+          <div>
+            <h2 className="text-blue-900 text-xl font-bold mb-2">Mr.Nattapong Nakaom โพสต์งานใหม่: เขียนโปรแกรมไพธอน บวกเลข 1-10</h2>
+            <p className="text-gray-500 text-lg">โพสต์เมื่อ: 13 กุมภาพันธ์ 2029</p>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
-
-export default Home
--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,25 +1,19 @@
 'use client';
 import React, { useState } from "react";
-// import "./globals.css";
 import Sidebar from "../components/Sidebar";
 
-
-export default function RootLayout({
+export default function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
     const [isOpen, setIsOpen] = useState(true);
   return (
-    <html lang="en">
-      <body
-      className="flex min-h-screen"
-      >
+    <section className="flex min-h-screen">
         <Sidebar isOpen={isOpen} setIsOpen={setIsOpen}  />
         <div className="flex-grow p-5">
           {children}
         </div>
-      </body>
-    </html>
+    </section>
   );
 }
